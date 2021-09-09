952944d4089b4d3e4567812c046ecef6931f576f
azkalbhqi <azkalpredator@gmail.com>
2021-09-09 22:10:51 +0700

    third commit

--- script.js
+++ script.js
@@ -53,13 +53,6 @@ function equalcrc(){
 // responsive navbar //
 
 
-    // const menuToggle = document.querySelector('.menu-toggle input');
-    // const nav = document.querySelector('nav ul');
-
-    // menuToggle.addEventListener('click' , function (){
-    // nav.classList.toggle('slide');
-
-    // });
 function navbar(){
     const menuToggle = document.querySelector('.menu-toggle input');
     const nav = document.querySelector('nav ul')
